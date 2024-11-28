840f8d1e95532d031ab3b574621a4393064ae6d2
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-11-28 19:37:26 +0900

    feat: 각 카드 헤더 추가

--- src/app/trade/[symbol]/_components/OrderBook/OrderBookItemHeader.tsx
+++ src/app/trade/[symbol]/_components/OrderBook/OrderBookItemHeader.tsx
@@ -1,6 +1,6 @@
 export default function OrderBookItemHeader() {
   return (
-    <ul className="flex justify-between text-xs text-[var(--color-TertiaryText)] font-semibold mb-3">
+    <ul className="flex justify-between text-xs text-[var(--color-TertiaryText)] font-semibold">
       <li>Price (USDT)</li>
       <li className="flex-1 text-right">Amount (BTC)</li>
       <li className="w-24 shrink-0 text-right">Total</li>
