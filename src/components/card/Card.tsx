2711d3b768ef0f24569a4709bba5439808f5ada4
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-11-28 19:12:45 +0900

    feat: OrderForm 퍼블리싱 (#7)
    * feat: tabs 컴포넌트 분리
    
    * feat: Input 컴포넌트 추가
    
    * feat: Buttons 컴포넌트 추가

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { createContext, PropsWithChildren, ReactNode, useContext } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 
 interface CardWrapperProps {
   className?: string;
@@ -23,85 +22,6 @@ function SubHeader({ children }: { children: ReactNode }) {
   );
 }
 
-const TabListContext = createContext<{
-  selected: string;
-  setSelected: (selected: string) => void;
-  list: string[];
-  subHeader?: boolean;
-}>({ selected: "", setSelected: () => {}, list: [], subHeader: false });
-
-function Tabs({
-  className,
-  children,
-  ...props
-}: {
-  list: string[];
-  selected: string;
-  setSelected: (selected: string) => void;
-  subHeader?: boolean;
-  className?: string;
-  children?: ReactNode;
-}) {
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    const target = e.target as HTMLDivElement;
-    props.setSelected(target.innerText);
-  };
-
-  return (
-    <TabListContext.Provider value={props}>
-      <div
-        className={`h-full flex ${
-          props.subHeader ? "gap-2" : "gap-6"
-        } ${className}`}
-        onClick={(e) => handleClick(e)}
-      >
-        {children}
-      </div>
-    </TabListContext.Provider>
-  );
-}
-
-function TabList() {
-  const { selected, list, subHeader } = useContext(TabListContext);
-
-  const TabComponent = subHeader ? SubTab : Tab;
-
-  return (
-    <>
-      {list.map((item) => (
-        <TabComponent key={item} item={item} isSelected={selected === item} />
-      ))}
-    </>
-  );
-}
-
-function Tab({ item, isSelected }: { item: string; isSelected: boolean }) {
-  return (
-    <div
-      className={`relative flex items-center justify-between text-sm font-bold cursor-pointer text-[var(--color-TertiaryText)] hover:text-[var(--color-PrimaryText)] transition-colors duration-300 ${
-        isSelected ? "text-[var(--color-PrimaryText)]" : ""
-      }`}
-    >
-      {item}
-      {isSelected && (
-        <div className="w-4 absolute bottom-0 left-1/2 -translate-x-1/2 h-[3px] bg-[var(--color-PrimaryYellow)]" />
-      )}
-    </div>
-  );
-}
-
-function SubTab({ item, isSelected }: { item: string; isSelected: boolean }) {
-  return (
-    <div
-      className={`relative flex items-center justify-between text-xs font-bold cursor-pointer text-[var(--color-TertiaryText)] hover:text-[var(--color-PrimaryText)] transition-colors duration-300 ${
-        isSelected ? "text-[var(--color-PrimaryText)]" : ""
-      }`}
-    >
-      {item}
-    </div>
-  );
-}
-
 function Card({
   children,
   className,
@@ -110,19 +30,16 @@ function Card({
 }: PropsWithChildren<CardWrapperProps>) {
   return (
     <div
-      className={`bg-[var(--color-BasicBg)] rounded-md ${className}`}
+      className={`bg-[var(--color-BasicBg)] rounded-md ${className} flex flex-col`}
       style={style}
     >
       {header}
-      <div className="px-4 h-full">{children}</div>
+      <div className="px-4 flex-1">{children}</div>
     </div>
   );
 }
 
 Card.Header = Header;
 Card.SubHeader = SubHeader;
-
-Card.Tabs = Tabs;
-Card.TabList = TabList;
 
 export { Card };
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,101 @@
+import {
+  ComponentProps,
+  Dispatch,
+  forwardRef,
+  SetStateAction,
+  useState,
+} from "react";
+import InputArrowPad from "./InputNumberArrowPad";
+
+const InputLabel = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <p className="text-sm text-[var(--color-TertiaryText)] font-bold">
+      {children}
+    </p>
+  );
+};
+
+const InputField = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full h-10 px-2 relative flex items-center gap-2 rounded-md border-[1px] border-[var(--color-DisableText)] hover:border-[var(--color-PrimaryYellow)] transition-colors">
+      {children}
+    </div>
+  );
+};
+
+type InputProps<T extends string | number | readonly string[]> =
+  ComponentProps<"input"> & {
+    setValue: Dispatch<SetStateAction<T>> & ((value: T) => void);
+    unit?: string;
+  };
+
+const InputNumber = forwardRef<HTMLInputElement, InputProps<number>>(
+  ({ setValue, className, type, unit, ...props }, ref) => {
+    const [currentValue, setCurrentValue] = useState(0);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = Number(e.target.value);
+      setCurrentValue(value);
+      setValue(value);
+    };
+
+    return (
+      <>
+        <input
+          type={type}
+          className={`flex-1 bg-transparent text-[var(--color-PrimaryText)] text-sm font-black ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-right ${className}`}
+          ref={ref}
+          {...props}
+          value={currentValue}
+          onChange={handleInputChange}
+        />
+        {unit && <div className="font-black text-sm">{unit}</div>}
+
+        <InputArrowPad
+          step={Number(props.step)}
+          value={Number(currentValue ?? 0)}
+          setValue={setCurrentValue}
+        />
+      </>
+    );
+  }
+);
+
+const Input = forwardRef<
+  HTMLInputElement,
+  InputProps<string | number | readonly string[]>
+>(({ unit, setValue, className, type, ...props }, ref) => {
+  const [currentValue, setCurrentValue] = useState<
+    string | number | undefined | readonly string[]
+  >(props.defaultValue ?? undefined);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentValue(e.target.value);
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <input
+        type={type}
+        value={currentValue}
+        onChange={handleInputChange}
+        className={`flex-1 bg-transparent text-[var(--color-PrimaryText)] text-sm font-black ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ${
+          currentValue === 0 ? "text-transparent" : ""
+        } ${className}`}
+        ref={ref}
+        {...props}
+      />
+      {unit && <div className="font-black text-sm">{unit}</div>}
+    </>
+  );
+});
+
+InputField.Label = InputLabel;
+InputField.Input = Input;
+InputField.Number = InputNumber;
+
+Input.displayName = "Input";
+InputNumber.displayName = "InputNumber";
+
+export { InputField };
